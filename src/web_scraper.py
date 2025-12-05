19df7b74dd273e9b98a61e4d79df181989027369
Kayky Gibran <72230429+KGMats@users.noreply.github.com>
2025-12-05 14:52:25 -0300

    Fixed config module added config.ini to gitignore

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup #bibioteca para filtrar o texto do site
 import requests #biblioteca para pegar o texto do site
 import json
 import os
+from config import config
+
 
 #link do site a ser utilizado
-url = 'https://sistemas.prefeitura.unicamp.br/apps/cardapio/index.php' # ...index.php?d=2025-12-01
+# ...index.php?d=2025-12-01
 
 #retrieves data from a web server.
-page = requests.get(url) 
+page = requests.get(config.url_cardapio) 
 
 #html do site "print(soup.prettify()"
 soup = BeautifulSoup(page.text, 'html.parser') 
@@ -33,7 +35,7 @@ def get_week_menu(dias):
     week = {}
     for key, value in dias.items():
         dia_da_semana = key
-        url = f'https://sistemas.prefeitura.unicamp.br/apps/cardapio/index.php{value}'
+        url = f'{config.url_cardapio}/index.php{value}'
         dia = {} 
 
         pagina_atual = requests.get(url)
