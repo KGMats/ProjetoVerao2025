58c6bb7afe019e7bb846589d1d30e3ec79a1f119
Yanetti <matheusyanetti@gmail.com>
2025-12-04 22:03:22 -0300

    import main teste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # logica central de chamar a api, salvar ela em data (para nao ter que fzr o web scrapping duas
 # vezes por dia), depois linkar ela com a classe, e mandar para o zap.
 
-from src.web_scraper import save_weekly_data, get_week_menu
-#from src.whatsapp import 
-
+from src.init import save_weekly_data, get_week_menu 
