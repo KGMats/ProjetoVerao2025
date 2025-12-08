8697cbfbf29cb1ff668b838d00f92478c6dc8091
Bernardo Arantes <bernardoarantes2006@gmail.com>
2025-12-08 19:43:33 +0300

    src/whatsapp.py

--- src/whatsapp.py
+++ src/whatsapp.py
@@ -1,9 +1,6 @@
 # codigo para enviar a mensagem formatada 
 
-# codigo para enviar a mensagem formatada
-
 import json
-from pywhatkit import sendwhatmsg_to_group
 from random import choice
 from datetime import date
 from urllib.parse import quote
@@ -13,9 +10,8 @@ from selenium.webdriver.common.by import By
 import pickle
 import time
 
-def create_msg(refeição: dict, classing: dict, msg_dict: dict):
-    msg = ""
-    
+def create_msg(refeição: dict, classing: dict, msg_dict: dict, msg: str = ""):
+
     proteina = refeição["proteina"]
     guarnicao = refeição["guarnicao"]
     salada = refeição["salada"]
@@ -57,31 +53,31 @@ def send_msg(msg: str):
     driver = wb.Chrome(options=options)
     driver.get(f"https://web.whatsapp.com/")
     load_cookies(driver)
-    driver.get(f"https://web.whatsapp.com/send?&text={quote("victor viado")}")
-    aa = True
-    while aa:
+    driver.get(f"https://web.whatsapp.com/send?&text={quote(msg)}")
+    found = False
+    while not found:
         try:
             box = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/span[2]/div/div/div/div/div/div/div/div[2]/div/div/div/div[3]/div/div[2]/div/div[2]')
-            aa = False
+            found = True
         except:
             pass
     box.click()
     
-    bb = True
-    while bb:
+    found = False
+    while not found:
         try:
             forward_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/span[2]/div/div/div/div/div/div/div/span/div/div/div')
-            bb = False
+            found = True
         except:
             pass
     time.sleep(2)
     forward_button.click()
 
-    cc = True
-    while cc:
+    found = False
+    while not found:
         try:
             send_button = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div/div[4]/div/span/button')
-            cc = False
+            found = True
         except:
             pass
     time.sleep(2)
@@ -102,52 +98,16 @@ def save_and_exit(driver):
     driver.close()
     exit()
 
-# def excessoes(comida: str, code: int, h: int, m: int):
-#     # code 0 representa almoço, 1 é janta
-#     chave = "almoco"
-#     if code == 1:
-#         chave = "jantar"
-        
-#     if ref[diAtual]["almoco"]["proteina"][:5] == "PEIXE":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["proteina"]["peixe"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["proteina"][:8] == "FEIJOADA":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["proteina"]["feijoada"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["proteina"][:9] == "SOBRECOXA":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["proteina"]["sobrecoxa"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["proteina"][:6] == "NUGGET":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["proteina"]["nugget"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["guarnicao"][:8] == "MACARRÃO":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["guarnicao"]["macarrao"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["suco"][12:] == "MANGA":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["refresco"]["manga"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["suco"][12:] == "LIMÃO":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["refresco"]["limao"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["proteina"][:7] == "LARANJA":
-#         sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["sobremesa"]["laranja"])}""", h, m, 10)
-#     elif ref[diAtual]["almoco"]["proteina"][:7] == "MELANCIA":
-        # sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{choice(msgs["sobremesa"]["melancia"])}""", h, m, 10)
 
-def enviarRef(comida: str, code: int, h: int, m: int):
-    if code == 0: # code 0 representa almoço, 1 é janta
-        sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{comida}""", h, m, 10)
-        return
-    sendwhatmsg_to_group("BmrEqCXJkUTHlEiZhCifUD", f"""{comida}""", h, m, 10)
 
-# diAtual = date.today().isoformat()  #classe data do datetime que tem o método today, isoformat é o formato da data na chave do dict
-# with open("../weekly-quotes.json", "r", encoding= "utf8") as f: #abrir arquivo padrão
-#     msgs = json.load(f)
 
-# with open("../weekly-data.json", "r", encoding= "utf8") as f:
-#     ref = json.load(f)
 
-# enviarRef(f"""{ref[diAtual]["almoco"]}""", 0, 10, 0)
-# excessoes(f"""{ref[diAtual]["almoco"]}""", 0, 10, 0)
-# try:
-#     enviarRef(f"""{ref[diAtual]["jantar"]}""", 1, 15, 0)
-#     excessoes(f"""{ref[diAtual]["jantar"]}""", 1, 15, 5)
 
-# #Domingo e feriados não tem janta
-# except KeyError:
-#     pass
 
-send_msg("msg")
+
+
+
+
+
+
+
