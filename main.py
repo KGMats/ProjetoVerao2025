062eb3a06dc3505eca608bcf97f4727ed2f7749d
Yanetti <matheusyanetti@gmail.com>
2025-12-08 15:52:18 -0300

    transferencia logica main e rm schedule.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # logica central de chamar a api, salvar ela em data (para nao ter que fzr o web scrapping duas
 # vezes por dia), depois linkar ela com a classe, e mandar para o zap.
+# agendamento de mensagem atraves de bibliotecas como a prorpia schedule (chama web-scraper e whatsapp)
 
-from src.init import save_weekly_data, get_week_menu 
+import schedule
+import time
+from init import save_weekly_data
+
+schedule.every().monday.at("10:00").do(save_weekly_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
